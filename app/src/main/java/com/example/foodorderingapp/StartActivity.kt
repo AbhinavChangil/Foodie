bc7f01157a59932ab4982e64388570229d2aa87d
AbhinavChangil <e21cseu0910@bennett.edu.in>
2024-07-10 13:06:46 +0530

    Start Screen

--- app/src/main/java/com/example/foodorderingapp/StartActivity.kt
+++ app/src/main/java/com/example/foodorderingapp/StartActivity.kt
@@ -1,28 +1,20 @@
 package com.example.foodorderingapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SplashScreen : AppCompatActivity() {
+class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_start)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        Handler().postDelayed({
-            val intent = Intent(this, StartActivity::class.java)
-            startActivity(intent)
-            finish()
-        },3000)
     }
 }
